572bdbfb4b6e4477d470f3f614b96856fed5004c
bvillasen <bvillasen@gmail.com>
2014-05-04 00:26:25 -0500

    external activiy finder added

--- cudaPercolation2D.cu
+++ cudaPercolation2D.cu
@@ -7,32 +7,6 @@ typedef unsigned char uchar;
 
 texture< int, cudaTextureType2D, cudaReadModeElementType> tex_isFree;
 texture<float, cudaTextureType2D, cudaReadModeElementType> tex_concentrationIn;
-// surface< void, cudaSurfaceType2D> surf_concentrationOut;
-
-// __global__ void countFreeNeighbors_kernel( const int nWidth, const int nHeight, int *nFreeAll){
-//   int t_j = blockIdx.x*blockDim.x + threadIdx.x;
-//   int t_i = blockIdx.y*blockDim.y + threadIdx.y;
-//   int tid = t_j + t_i*blockDim.x*gridDim.x;
-//   
-//   uchar left   = tex2D( tex_isFree, t_j-1, t_i );
-//   uchar right  = tex2D( tex_isFree, t_j+1, t_i );
-//   uchar up     = tex2D( tex_isFree, t_j, t_i+1 );
-//   uchar down   = tex2D( tex_isFree, t_j, t_i-1 );
-//   
-//   //Set PERIODIC boundary conditions
-//   if (t_i == 0)           down = tex2D( tex_isFree, t_j, nHeight-1 );
-//   if (t_i == (nHeight-1))   up = tex2D( tex_isFree, t_j, 0 );
-//   if (t_j == 0)           left = tex2D( tex_isFree, nWidth-1, t_i );
-//   if (t_j == (nWidth-1)) right = tex2D( tex_isFree, 0, t_i );
-//   
-//   int nFree = 0;
-//   if ( left )  nFree += 1;
-//   if ( right ) nFree += 1;
-//   if ( down )  nFree += 1;
-//   if ( up )    nFree += 1;
-// 
-//   nFreeAll[tid] = nFree;
-// }
 /////////////////////////////////////////////////////////////////////////////////////// 
 /////////////////////////////////////////////////////////////////////////////////////// 
 __global__ void main_kernel_tex( const int nWidth, const int nHeight, float hx, int *isFreeAll,
@@ -64,12 +38,9 @@ __global__ void main_kernel_tex( const int nWidth, const int nHeight, float hx, 
   if (t_j == 0)           left_C = tex2D( tex_concentrationIn, nWidth-1, t_i );
   if (t_j == (nWidth-1)) right_C = tex2D( tex_concentrationIn, 0, t_i );
  
-  float newConcentration = 0.25f*( left_C + right_C + down_C + up_C ) +
-         0.25f*( 4 - ( left_isFree + right_isFree + down_isFree + up_isFree ) )*center_C;
-
-//   float newConcentration = hx*left_C + (1.f - hx)*(right_C + down_C + up_C )/3.f +
-//       ( hx*(1 - right_isFree) + (1.f-hx)*( 3 - ( left_isFree + down_isFree + up_isFree ) )/3.f )*center_C;
-//       
+  float newConcentration = hx*left_C + (1.f - hx)*(right_C + down_C + up_C )/3.f +
+      ( hx*(1 - right_isFree) + (1.f-hx)*( 3 - ( left_isFree + down_isFree + up_isFree ) )/3.f )*center_C;
+      
   if ( isFreeAll[tid] )  concentrationOut[tid] = newConcentration;
 }
 
@@ -79,10 +50,15 @@ __global__ void main_kernel_tex( const int nWidth, const int nHeight, float hx, 
 /////////////////////////////////////////////////////////////////////////////////////// 
 /////////////////////////////////////////////////////////////////////////////////////// 
 __global__ void main_kernel_shared( const int nWidth, const int nHeight, cudaP hx, uchar *isFreeAll,
-			          cudaP *concIn, cudaP *concentrationOut ){
+			          cudaP *concIn, cudaP *concentrationOut, uchar *activeBlocks ){
   const int t_j = blockIdx.x*blockDim.x + threadIdx.x;
   const int t_i = blockIdx.y*blockDim.y + threadIdx.y;
   const int tid = t_j + t_i*blockDim.x*gridDim.x;
+  
+//   __shared__ uchar activeBlock;
+//   if ( threadIdx.x == 0 and threadIdx.y ==0 ) activeBlock = activeBlocks[blockIdx.x + blockIdx.y*gridDim.x ];
+//   __syncthreads();
+//   if ( !activeBlock ) return;
   
   //Read my neighbors concentration
   __shared__ uchar   isFree_sh[ %(B_WIDTH)s + 2 ][ %(B_HEIGHT)s + 2 ];
@@ -128,151 +104,68 @@ __global__ void main_kernel_shared( const int nWidth, const int nHeight, cudaP h
   __syncthreads();
   	 
   cudaP oneThird = 1.0/3;
-  
-//   cudaP newConc = hx*conc_sh[threadIdx.x][threadIdx.y+1] + 
-//     (1 - hx)*( conc_sh[threadIdx.x+2][threadIdx.y+1] + conc_sh[threadIdx.x+1][threadIdx.y] + conc_sh[threadIdx.x+1][threadIdx.y+2] )*oneThird +
-//     ( hx*(1 - isFree_sh[threadIdx.x+2][threadIdx.y+1]) + 
-//       (1 - hx)*( 3 - ( isFree_sh[threadIdx.x][threadIdx.y+1] + isFree_sh[threadIdx.x+1][threadIdx.y] + isFree_sh[threadIdx.x+1][threadIdx.y+2] ) )*oneThird )*conc_sh[threadIdx.x+1][threadIdx.y+1];
 
-  cudaP newConc = hx*( conc_sh[threadIdx.x][threadIdx.y+1] + ( 1 - isFree_sh[threadIdx.x+2][threadIdx.y+1] )*conc_sh[threadIdx.x+1][threadIdx.y+1] ) +
+//   cudaP newConc = hx*( conc_sh[threadIdx.x][threadIdx.y+1] + ( 1 - isFree_sh[threadIdx.x+2][threadIdx.y+1] )*conc_sh[threadIdx.x+1][threadIdx.y+1] ) +
+//     oneThird*( 1 - hx )*( conc_sh[threadIdx.x+2][threadIdx.y+1] + conc_sh[threadIdx.x+1][threadIdx.y] + conc_sh[threadIdx.x+1][threadIdx.y+2] +
+//                          conc_sh[threadIdx.x+1][threadIdx.y+1]*( 3 - ( isFree_sh[threadIdx.x][threadIdx.y+1] + isFree_sh[threadIdx.x+1][threadIdx.y] + isFree_sh[threadIdx.x+1][threadIdx.y+2] ) ) );
+    
+  if ( isFree_sh[threadIdx.x+1][threadIdx.y+1] )  concentrationOut[tid] =
+	   hx*( conc_sh[threadIdx.x][threadIdx.y+1] + ( 1 - isFree_sh[threadIdx.x+2][threadIdx.y+1] )*conc_sh[threadIdx.x+1][threadIdx.y+1] ) +
     oneThird*( 1 - hx )*( conc_sh[threadIdx.x+2][threadIdx.y+1] + conc_sh[threadIdx.x+1][threadIdx.y] + conc_sh[threadIdx.x+1][threadIdx.y+2] +
                          conc_sh[threadIdx.x+1][threadIdx.y+1]*( 3 - ( isFree_sh[threadIdx.x][threadIdx.y+1] + isFree_sh[threadIdx.x+1][threadIdx.y] + isFree_sh[threadIdx.x+1][threadIdx.y+2] ) ) );
     
-  if ( isFree_sh[threadIdx.x+1][threadIdx.y+1] )  concentrationOut[tid] = newConc;
-}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// __global__ void findActivity_kernel( cudaP minVal, cudaP *concentration, uchar *activeBlocks ){
-//   int t_j = blockIdx.x*blockDim.x + threadIdx.x;
-//   int t_i = blockIdx.y*blockDim.y + threadIdx.y;
-//   int tid = t_j + t_i*blockDim.x*gridDim.x;
-//   int tid_b = threadIdx.x + threadIdx.y*blockDim.x;
-// 
-//   __shared__ cudaP concentration_sh[ %(THREADS_PER_BLOCK)s ];
-//   concentration_sh[tid_b] = concentration[tid];
-//   __syncthreads();
-//   
-//   int i = blockDim.x*blockDim.y / 2;
-//   while ( i > 0 ){
-//     if ( tid_b < i ) concentration_sh[tid_b] = concentration_sh[tid_b] + concentration_sh[tid_b+i];
-//     __syncthreads();
-//     i /= 2;
-//   }
-//   if ( tid_b == 0 ){
-//     if (concentration_sh[0] >= minVal ) {
-//       activeBlocks[ blockIdx.x + blockIdx.y*gridDim.x ] = (uchar) 1;
-//       //right 
-//       if (blockIdx.x < gridDim.x-1) activeBlocks[ (blockIdx.x+1) + blockIdx.y*gridDim.x ] = (uchar) 1;
-//       //left
-//       if (blockIdx.x > 0) activeBlocks[ (blockIdx.x-1) + blockIdx.y*gridDim.x ] = (uchar) 1;
-//       //up 
-//       if (blockIdx.y < gridDim.y-1) activeBlocks[ blockIdx.x + (blockIdx.y+1)*gridDim.x  ] = (uchar) 1;
-//       //Down 
-//       if (blockIdx.y > 0) activeBlocks[ blockIdx.x + (blockIdx.y-1)*gridDim.x  ] = (uchar) 1;
-//     }
-//   }
-// }
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// __global__ void getActivity_kernel(  uchar *activeBlocks, uchar *activeThreads ){
-//   int t_j = blockIdx.x*blockDim.x + threadIdx.x;
-//   int t_i = blockIdx.y*blockDim.y + threadIdx.y;
-//   int tid = t_j + t_i*blockDim.x*gridDim.x ;
-//   int tid_b = threadIdx.x + threadIdx.y*blockDim.x;
-//   int bid = blockIdx.x + blockIdx.y*gridDim.x;
-//   
-//   __shared__ uchar activeBlock;
-//   if (tid_b == 0 ) activeBlock = activeBlocks[bid];
-//   __syncthreads();
-//   
-//   if ( activeBlock ) activeThreads[tid] = (uchar) 1;
-//   else activeThreads[tid] = (uchar) 0;
-// }
 
-/*
-__global__ void main_kernel( const int nWidth, const int nHeight, int *nFreeAll, uchar *isFree, cudaP *concentrationIn, cudaP *concentrationOut ){
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+__global__ void findActivity_kernel( cudaP minVal, cudaP *concentration, uchar *activeBlocks ){
   int t_j = blockIdx.x*blockDim.x + threadIdx.x;
   int t_i = blockIdx.y*blockDim.y + threadIdx.y;
   int tid = t_j + t_i*blockDim.x*gridDim.x;
-  
-  cudaP localConc = concentrationIn[tid];
-  
-  __shared__ uchar isFree_sh[ %(B_WIDTH)s + 2 ][ %(B_HEIGHT)s + 2 ];
-  __shared__ cudaP concentrationIn_sh[ %(B_WIDTH)s + 2 ][ %(B_HEIGHT)s + 2 ];
-  __shared__ cudaP concentrationOut_sh[ %(B_WIDTH)s + 2 ][ %(B_HEIGHT)s + 2 ];
-  isFree_sh[threadIdx.x+1][threadIdx.y+1] = isFree[tid]; 
-  concentrationIn_sh[threadIdx.x+1][threadIdx.y+1] = localConc ;
-  concentrationOut_sh[threadIdx.x+1][threadIdx.y+1] = cudaP(0.);
-  
-  //Left boundary
-  if (t_j == 0){  //Set periodic boundary
-    isFree_sh[0][threadIdx.y+1] = isFree[ (nWidth-1) + t_i*nWidth ];
-    concentrationIn_sh[0][threadIdx.y+1] = concentrationIn[ (nWidth-1) + t_i*nWidth ];
-    concentrationOut_sh[0][threadIdx.y+1] = cudaP(0.);
-  }
-  else if ( threadIdx.x == 0 ){
-    isFree_sh[0][threadIdx.y+1] = isFree[ (t_j-1) + t_i*nWidth ];
-    concentrationIn_sh[0][threadIdx.y+1] = concentrationIn[ (t_j-1) + t_i*nWidth ];
-    concentrationOut_sh[0][threadIdx.y+1] = cudaP(0.);
-  }
-  //Right boundary
-  if (t_j == nWidth-1){  //Set periodic boundary
-    isFree_sh[blockDim.x+1][threadIdx.y+1] = isFree[ t_i*nWidth ];
-    concentrationIn_sh[blockDim.x+1][threadIdx.y+1] = concentrationIn[ t_i*nWidth ];
-    concentrationOut_sh[blockDim.x+1][threadIdx.y+1] = cudaP(0.);
-  }
-  else if ( threadIdx.x == blockDim.x-1 ){
-    isFree_sh[blockDim.x+1][threadIdx.y+1] = isFree[ (t_j+1) + t_i*nWidth ];
-    concentrationIn_sh[blockDim.x+1][threadIdx.y+1] = concentrationIn[ (t_j+1) + t_i*nWidth ];
-    concentrationOut_sh[blockDim.x+1][threadIdx.y+1] = cudaP(0.);
-  }
-  //Down boundary
-  if (t_i == 0){  //Set periodic boundary
-    isFree_sh[threadIdx.x+1][0] = isFree[ t_j + (nHeight-1)*nWidth ];
-    concentrationIn_sh[threadIdx.x+1][0] = concentrationIn[ t_j + (nHeight-1)*nWidth ];
-    concentrationOut_sh[threadIdx.x+1][0] = cudaP(0.);
-  }
-  else if ( threadIdx.y == 0 ){
-    isFree_sh[threadIdx.x+1][0] = isFree[ t_j + (t_i-1)*nWidth ];
-    concentrationIn_sh[threadIdx.x+1][0] = concentrationIn[ t_j + (t_i-1)*nWidth ];
-    concentrationOut_sh[threadIdx.x+1][0] = cudaP(0.);
-  }
-  //Up boundary
-  if (t_i == nHeight-1){  //Set periodic boundary
-    isFree_sh[threadIdx.x+1][blockDim.y+1] = isFree[ t_j ];
-    concentrationIn_sh[threadIdx.x+1][blockDim.y+1] = concentrationIn[ t_j ];
-    concentrationOut_sh[threadIdx.x+1][blockDim.y+1] = cudaP(0.);
-  }
-  else if ( threadIdx.y == blockDim.y-1 ){
-    isFree_sh[threadIdx.x+1][blockDim.y+1] = isFree[ t_j + (t_i+1)*nWidth ];
-    concentrationIn_sh[threadIdx.x+1][blockDim.y+1] = concentrationIn[ t_j + (t_i+1)*nWidth ];
-    concentrationOut_sh[threadIdx.x+1][blockDim.y+1] = cudaP(0.);
-  }
+  int tid_b = threadIdx.x + threadIdx.y*blockDim.x;
+
+  __shared__ cudaP concentration_sh[ %(THREADS_PER_BLOCK)s ];
+  concentration_sh[tid_b] = concentration[tid];
   __syncthreads();
   
-  int nFree = nFreeAll[tid];
-//   concentrationOut[tid] = nFree;
-  cudaP sendVal = localConc/nFree;
-  if (isFree_sh[threadIdx.x][threadIdx.y + 1])     atomicAdd( &(concentrationOut_sh[threadIdx.x][threadIdx.y + 1]) , sendVal);      //Left 	
-  if (isFree_sh[threadIdx.x + 2][threadIdx.y + 1]) atomicAdd( &(concentrationOut_sh[threadIdx.x + 2][threadIdx.y + 1]) , sendVal);  //Right
-  if (isFree_sh[threadIdx.x + 1][threadIdx.y])     atomicAdd( &(concentrationOut_sh[threadIdx.x + 1][threadIdx.y]) , sendVal);      //Down
-  if (isFree_sh[threadIdx.x + 1][threadIdx.y + 2]) atomicAdd( &(concentrationOut_sh[threadIdx.x + 1][threadIdx.y + 2]) , sendVal);  //Up
-  __syncthreads();
-//   if ( isFree_sh[threadIdx.x + 1][threadIdx.y + 1] ){
-  //Write shared memory to global
-  concentrationOut[tid] = concentrationOut_sh[threadIdx.x+1][threadIdx.y+1];
-  //Write shared memory boundary
-  //Left boundary
-  if (t_j == 0)                atomicAdd( &(concentrationOut[ (nWidth-1) + t_i*nWidth ]), concentrationOut_sh[0][threadIdx.y + 1] );
-  else if ( threadIdx.x == 0 ) atomicAdd( &(concentrationOut[ (t_j-1) + t_i*nWidth ]), concentrationOut_sh[0][threadIdx.y + 1] );
-  //Rigth boundary
-  if (t_j == nWidth-1)                    atomicAdd( &(concentrationOut[ t_i*nWidth ]), concentrationOut_sh[blockDim.x + 1][threadIdx.y + 1] );
-  else if ( threadIdx.x == blockDim.x-1 ) atomicAdd( &(concentrationOut[ (t_j+1) + t_i*nWidth ]), concentrationOut_sh[blockDim.x + 1][threadIdx.y + 1] );
-  //Down boundary
-  if (t_i == 0)                atomicAdd( &(concentrationOut[ t_j + (nHeight-1)*nWidth ]), concentrationOut_sh[threadIdx.x + 1][0] );
-  else if ( threadIdx.y == 0 ) atomicAdd( &(concentrationOut[ t_j + (t_i-1)*nWidth ]), concentrationOut_sh[threadIdx.x + 1][0] );
-  //Up boundary
-  if (t_i == nHeight-1)                    atomicAdd( &(concentrationOut[ t_j ]), concentrationOut_sh[threadIdx.x + 1][ blockDim.y + 1] );
-  else if ( threadIdx.y == blockDim.y-1 )  atomicAdd( &(concentrationOut[ t_j + (t_i+1)*nWidth ]), concentrationOut_sh[threadIdx.x + 1][blockDim.y + 1] );
+  int i = blockDim.x*blockDim.y / 2;
+  while ( i > 0 ){
+    if ( tid_b < i ) concentration_sh[tid_b] = concentration_sh[tid_b] + concentration_sh[tid_b+i];
+    __syncthreads();
+    i /= 2;
+  }
+  if (concentration_sh[0] >= minVal ){
+    if  ( tid_b < 3 ){
+      // left,  center and right
+      if ( ( blockIdx.x > 0 ) and ( blockIdx.x < gridDim.x-1 ) ) activeBlocks[ blockIdx.x + (tid_b-1) + blockIdx.y*gridDim.x ] = (uchar) 1;
+      // down and up
+      if ( ( tid_b != 1) and (blockIdx.y > 0) and ( blockIdx.y < gridDim.y-1 ) ) activeBlocks[ blockIdx.x + (blockIdx.y+tid_b-1)*gridDim.x  ] = (uchar) 1;
+//       //right 
+//       if (blockIdx.x < gridDim.x-1) activeBlocks[ blockIdx.x+1 + blockIdx.y*gridDim.x ] = (uchar) 1;
+//       //left
+//       if (blockIdx.x > 0) activeBlocks[ (blockIdx.x-1) + blockIdx.y*gridDim.x ] = (uchar) 1;
+//       if ( tid_b == 0 ){
+//       //up 
+// 	if (blockIdx.y < gridDim.y-1) activeBlocks[ blockIdx.x + (blockIdx.y+1)*gridDim.x  ] = (uchar) 1;
+// 	//Down 
+// 	if (blockIdx.y > 0) activeBlocks[ blockIdx.x + (blockIdx.y-1)*gridDim.x  ] = (uchar) 1;
+//       }
+    }
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+__global__ void getActivity_kernel(  uchar *activeBlocks, uchar *activeThreads ){
+  int t_j = blockIdx.x*blockDim.x + threadIdx.x;
+  int t_i = blockIdx.y*blockDim.y + threadIdx.y;
+  int tid = t_j + t_i*blockDim.x*gridDim.x ;
+  int tid_b = threadIdx.x + threadIdx.y*blockDim.x;
+  int bid = blockIdx.x + blockIdx.y*gridDim.x;
   
-}*/
+  __shared__ uchar activeBlock;
+  if (tid_b == 0 ) activeBlock = activeBlocks[bid];
+  __syncthreads();
+  uchar active = 0;
+  if ( activeBlock ) active = (uchar) 1;
+  activeThreads[tid] = active;
+}
--- percolation2D.py
+++ percolation2D.py
@@ -35,12 +35,13 @@ for option in sys.argv:
   if option.find("anim") >=0: usingAnimation = True
   if option.find("plot") >=0: plotting = True
   if option.find("p=") >=0: probability = float(option[option.find("=")+1:])
+  if option.find("h=") >=0: hx = float(option[option.find("=")+1:])
 precision  = {"float":np.float32, "double":np.float64} 
 cudaPre = precision[cudaP]
   
 #set simulation dimentions 
 nWidth = nPoints
-nHeight = nPoints / 4
+nHeight = nPoints / 2
 
 nCenter = 1
 offsetX = -nWidth/2 + 128
@@ -49,6 +50,7 @@ offsetY = 0
 nIterationsPerPlot = 500
 maxVals = []
 sumConc = []
+showActivity = False
 
 #Initialize openGL
 if usingAnimation:
@@ -65,6 +67,7 @@ gridx = nWidth // block_size_x + 1 * ( nWidth % block_size_x != 0 )
 gridy = nHeight // block_size_y + 1 * ( nHeight % block_size_y != 0 )
 block2D = (block_size_x, block_size_y, 1)
 grid2D = (gridx, gridy, 1)
+nBlocks = grid2D[0] + grid2D[1]
 
 #Read and compile CUDA code
 print "\nCompiling CUDA code\n"
@@ -73,8 +76,8 @@ cudaCodeString = cudaCodeString_raw  % { "THREADS_PER_BLOCK":block2D[0]*block2D[
 cudaCode = SourceModule(cudaCodeString)
 mainKernel_tex = cudaCode.get_function("main_kernel_tex" )
 mainKernel_sh = cudaCode.get_function("main_kernel_shared" )
-#findActivityKernel = cudaCode.get_function( "findActivity_kernel" )
-#getActivityKernel = cudaCode.get_function( "getActivity_kernel" )
+findActivityKernel = cudaCode.get_function( "findActivity_kernel" )
+getActivityKernel = cudaCode.get_function( "getActivity_kernel" )
 tex_isFree = cudaCode.get_texref('tex_isFree')
 tex_concentrationIn = cudaCode.get_texref('tex_concentrationIn')
 if showKernelMemInfo: 
@@ -85,15 +88,9 @@ if showKernelMemInfo:
 ########################################################################
 from pycuda.elementwise import ElementwiseKernel
 ########################################################################
-scalePlotData = ElementwiseKernel(arguments="cudaP a,  cudaP *realArray".replace("cudaP", cudaP),
-				operation = "realArray[i] = log10( 1 + (a*realArray[i] ) ) ",
+scalePlotData = ElementwiseKernel(arguments="cudaP a,  cudaP *realArray, unsigned char showActivity, unsigned char *activeThreads".replace("cudaP", cudaP),
+				operation = "realArray[i] = log10( 1 + (a*realArray[i] ) ) + activeThreads[i]*showActivity ; ",
 				name = "multiplyByScalarReal_kernel")
-###########################################################################
-#def countFreeNeighbors():
-  #tex_isFree.set_array( isFree_dArray )
-  #tex_nNeighb.set_array( nNeighb_dArray )	
-  #countFreeNeighborsKernel( np.int32(nWidth), np.int32(nHeight), nNeighb_d, grid=grid2D, block=block2D )
-  #copy2D_nNeigdbArray( aligned=True )
 ###########################################################################
 nIter = 0
 def oneIteration_tex():
@@ -104,20 +101,26 @@ def oneIteration_tex():
   nIter += 1
 def oneIteration_sh():
   global nIter
+  #cuda.memset_d8(activeBlocks_d.ptr, 0, nBlocks )
+  #findActivityKernel( cudaPre(1.e-30), concentrationIn_d, activeBlocks_d, grid=grid2D, block=block2D  )
   mainKernel_sh( np.int32(nWidth), np.int32(nHeight), cudaPre(hx), isFree_d, concentrationIn_d, concentrationOut_d,
-		grid=grid2D, block=block2D )
+		activeBlocks_d, grid=grid2D, block=block2D )
+  #cuda.memset_d8(activeBlocks_d.ptr, 0, nBlocks )
+  #findActivityKernel( cudaPre(1.e-30), concentrationOut_d, activeBlocks_d, grid=grid2D, block=block2D  )
   mainKernel_sh( np.int32(nWidth), np.int32(nHeight), cudaPre(hx), isFree_d, concentrationOut_d, concentrationIn_d,
-		grid=grid2D, block=block2D )
-  #cuda.memcpy_dtod( concentrationIn_d.ptr, concentrationOut_d.ptr, concentrationOut_d.nbytes )
-  #concentrationIn_d.gpudata, concentrationOut_d.gpudata = concentrationOut_d.gpudata, concentrationIn_d.gpudata 
+		activeBlocks_d, grid=grid2D, block=block2D )
   nIter += 1
 ###########################################################################
 animIter = 0
 def stepFunction():
   global animIter
+  if showActivity: 
+    cuda.memset_d8(activeBlocks_d.ptr, 0, nBlocks )
+    findActivityKernel( cudaPre(1.e-10), concentrationIn_d, activeBlocks_d, grid=grid2D, block=block2D  )
+    getActivityKernel( activeBlocks_d, activeThreads_d, grid=grid2D, block=block2D ) 
   cuda.memcpy_dtod( plotData_d.ptr, concentrationOut_d.ptr, concentrationOut_d.nbytes )
   maxVal = gpuarray.max( plotData_d ).get()
-  scalePlotData(100./maxVal, plotData_d)
+  scalePlotData(100./maxVal, plotData_d, np.uint8(showActivity), activeThreads_d )
   if cudaP == "float": [ oneIteration_tex() for i in range(nIterationsPerPlot) ]
   else: [ oneIteration_sh() for i in range(nIterationsPerPlot//2) ]
   if plotting and animIter%25 == 0: 
@@ -125,7 +128,17 @@ def stepFunction():
     sumConc.append( gpuarray.sum(concentrationIn_d).get() )
     plotData( maxVals, sumConc )
   animIter += 1
-  
+###########################################################################  
+def keyboardFunc(*args):
+  global showActivity
+  ESCAPE = '\033'
+  # If escape is pressed, kill everything.
+  if args[0] == ESCAPE:
+    print "Ending Simulation"
+    sys.exit()
+  if args[0] == "a":
+    showActivity = not showActivity
+    animation2D.maxVar += cudaPre(1.)
 def specialKeyboardFunc( key, x, y ):
   global hx
   if key== animation2D.GLUT_KEY_LEFT:
@@ -133,7 +146,6 @@ def specialKeyboardFunc( key, x, y ):
   if key== animation2D.GLUT_KEY_RIGHT:
     hx += 0.005
   animation2D.windowTitle = "Percolation 2D  grid={0}x{1}   p={2:.1f}     h={3:.3} ".format(nHeight, nWidth, float(probability), float(hx) )
-  
 ###########################################################################
 ###########################################################################
 #Initialize Data
@@ -155,8 +167,8 @@ else:
 isFree_d = gpuarray.to_gpu( isFree_h.astype(np.uint8) ) 
 concentrationIn_d = gpuarray.to_gpu( concentration_h )
 concentrationOut_d = gpuarray.to_gpu( concentration_h )
-#activeBlocks_d = gpuarray.to_gpu( np.zeros( [ grid2D[1],grid2D[0] ], dtype=np.uint8) )
-#activeThreads_d = gpuarray.to_gpu( np.zeros([nHeight, nWidth], dtype=np.uint8) )
+activeBlocks_d = gpuarray.to_gpu( np.zeros( [ grid2D[1],grid2D[0] ], dtype=np.uint8) )
+activeThreads_d = gpuarray.to_gpu( np.zeros([nHeight, nWidth], dtype=np.uint8) )
 #For texture version
 if cudaP == "float":
   isFree_dArray, copy2D_isFreeArray   = gpuArray2DtocudaArray( isFree_d )
@@ -172,6 +184,7 @@ print  " Total global memory used: {0:0.0f} MB\n".format( float(initialFreeMemor
 #configure animation2D functions and plotData
 if usingAnimation:
   animation2D.stepFunc = stepFunction
+  animation2D.keyboard = keyboardFunc
   animation2D.specialKeys = specialKeyboardFunc
   if cudaP == "double": animation2D.usingDouble = True
   animation2D.plotData_d = plotData_d
@@ -180,7 +193,6 @@ if usingAnimation:
   animation2D.maxVar = cudaPre(2.0045)
   animation2D.minVar = cudaPre(0.)
   animation2D.windowTitle = "Percolation 2D  grid={0}x{1}   p={2:.2f}     h={3:.3} ".format(nHeight, nWidth, float(probability), float(hx) )
-
 ###########################################################################
 ###########################################################################
 if showKernelMemInfo: 
